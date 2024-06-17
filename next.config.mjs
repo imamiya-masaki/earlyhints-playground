7a2833d16ed98f058a37603e209ca6c60ac4cafd
imamiya-masaki <imamiyagit@gmail.com>
2024-06-18 08:41:17 +0900

    earlyhintsようの検証環境用意

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/',
+                headers: [
+                  {
+                    key: 'Link',
+                    value: '<https://imamiya-masaki.github.io/thirdparty-js/deliver/third-party.js>; rel=preload',
+                  }
+                ],
+              },
+        ]
+    }
+};
 
 export default nextConfig;
