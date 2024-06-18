e36555d64bc0837935b9a54047a7145d25698cc5
imamiya-masaki <imamiyagit@gmail.com>
2024-06-18 09:31:46 +0900

    時間を増やす

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import styles from "./page.module.css";
-import Image from 'next/image'
 export default async function Home() {
 
   function sleep(milliseconds: number) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
   }
-  await sleep(3000)
+  await sleep(6000)
   return (
     <div className={styles.page}>
       <script async={true} src="https://imamiya-masaki.github.io/thirdparty-js/deliver/third-party.js"/>
-      読み込み
-      <img src={"/heavy-image.jpg"} width={5000} alt="heavy-image"></img>
+      HTML読み込み完了
+      <img src={"/heavy-image.jpg"} width={300} alt="heavy-image"></img>
     </div>
   );
 }
